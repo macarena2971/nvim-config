1f69aaab01d61f1bf36c847ac687db1229c55c34
Aleksandr Trushchinskii <macarena2971@proton.me>
2025-11-01 12:40:31 +0100

    Initial commit

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,27 @@
+return {
+  "mason-org/mason-lspconfig.nvim",
+  opts = {
+    ensure_installed = {
+      "lua_ls",
+      "dartls",
+    },
+    automatic_installation = true,
+  },
+  dependencies = {
+    {
+      "mason-org/mason.nvim",
+      opts = {
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+          }
+        }
+      }
+    },
+    {
+      "neovim/nvim-lspconfig",
+    }
+  },
+}
